8d32e3a08c05a238a47be0bfd255518fffa8b78b
Joy Majumdar <49155965+Joy-Majumder@users.noreply.github.com>
2024-08-13 03:43:07 +0600

    test

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-window.alert("Netflix Logo animation");
+function setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = `expires=${d.toUTCString()}`;
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
+
+// Example usage:
+setCookie('myCookieName', 'myCookieValue', 7); // Cookie lasts for 7 days
